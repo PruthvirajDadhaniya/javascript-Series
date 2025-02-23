7545b47f0e23de5dbb489d7f9e2036cb63a38a72
Pruthviraj Dadhaniya <pruthavirajdadhaniya@gmail.com>
2025-02-23 08:15:36 +0000

    conversion of datatype

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,73 @@
+let score = "Pruthviraj"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Pruthviraj"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 35
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// ***************************************** Operations *****************************************//
+
+let value = 5
+let negValue = -value
+
+console.log(negValue);
+
+console.log(4+4);
+console.log(4-4);
+console.log(4*4);
+console.log(4**4);
+console.log(4/4);
+console.log(4%4);
+
+
+let str1 ="Welcome"
+let str2 =" kolhapur"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+// console.log("1" + 3);
+// console.log(1 + "3");
+// console.log("1" + 3 + 3);
+// console.log(1 + 2 + "5");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let counter = 200
+counter++;
+++counter
+console.log(counter);
+console.log(counter);
+
+
+// more information on conversion documentattion link
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
