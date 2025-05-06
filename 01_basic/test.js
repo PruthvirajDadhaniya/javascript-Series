cb4fb135ac1114fb60d8c3ecc340b52f6de24c61
Pruthviraj Dadhaniya <pruthavirajdadhaniya@gmail.com>
2025-05-06 09:08:16 +0000

    arithematic operators

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,16 @@
 console.log("Pruthviraj");
+
+
+// ARITHEMATIC OPERATOR
+var a=10;
+var b=12;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+
+
+
+
