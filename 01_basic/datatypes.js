616b6328880d6bfc8de45ac39c4130c909a382e2
Pruthviraj Dadhaniya <pruthavirajdadhaniya@gmail.com>
2025-05-06 09:22:46 +0000

    primitive dadatype

--- 01_basic/datatypes.js
+++ 01_basic/datatypes.js
@@ -1,1 +1,14 @@
+// Datatype are uded to categories and represent different types of data
 
+//  primitive datatype
+// 1) string
+// 2) Nunmber
+// 3) boolean
+// 4) null
+// 5) undefined
+
+// 6) bigint
+// 7) symbol
+
+
+// Non-primitive/reference type
