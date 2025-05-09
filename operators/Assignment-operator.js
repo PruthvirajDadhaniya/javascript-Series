ded228196e43413f5f6bbe3da46ada884faf38ef
Pruthviraj Dadhaniya <pruthavirajdadhaniya@gmail.com>
2025-05-09 05:00:15 +0000

    Assignment-operator

---
+++ operators/Assignment-operator.js
@@ -0,0 +1,18 @@
+// Assignment operator
+
+var p=10;
+var q=10;
+var r=10;
+var s=10;
+var t=7;
+var u=2;
+
+console.log(p += q); // p= p + q =  20
+console.log(q -= r); // q= q - r =  0
+console.log(r *= s); // r= r * s =  100
+console.log(s /= t); // a= s / t =  3
+console.log(t %= u); // t= t % u =  1
+
+
+
+
