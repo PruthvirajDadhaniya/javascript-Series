8ffae711c6c139d26bbf4e071ea31eddce114743
Pruthviraj Dadhaniya <pruthavirajdadhaniya@gmail.com>
2025-05-09 03:50:15 +0000

    arithematic operator

---
+++ operators/Arithmatic-operator.js
@@ -0,0 +1,16 @@
+var a=20;
+var b=10;
+var c=4;
+var d=3;
+
+console.log(a + b); // 30
+console.log(a - b); // 10
+console.log(a * b); // 200
+console.log(a / b); // 2
+
+console.log(a % b); // 0 (Remainder)
+console.log(c ** d); // 4*4*4 = 64
+
+
+
+
