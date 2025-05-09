bb3d57543f44f9dbb85c1906e1ff8213065b329b
Pruthviraj Dadhaniya <pruthavirajdadhaniya@gmail.com>
2025-05-09 06:08:05 +0000

    Comparision-operator

---
+++ operators/Comparision-operator.js
@@ -0,0 +1,25 @@
+// COmparision-operator (Boolean)
+
+var p=20;
+var q=20;
+var r="20";
+var s=5;
+
+// == equal to                      (Check only value)           
+// === strictly equal to            (Chech values and datatype)    
+// != not equal to                  (Chech only values)
+// !== strictly not equal to        (Check values and datype)
+
+
+// < less than
+// <= less than equal to
+// > greater than
+// >= greater than equal to
+
+console.log(p == r); // true
+console.log(p === r);//false
+console.log(p != r);//false
+console.log(p !== r); //true
+
+
+
