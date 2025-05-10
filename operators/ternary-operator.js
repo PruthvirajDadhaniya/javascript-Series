c32e1748b3e112351dfb215b235cf06a9e25dc8f
Pruthviraj Dadhaniya <pruthavirajdadhaniya@gmail.com>
2025-05-10 08:32:17 +0000

    ternary-operator

---
+++ operators/ternary-operator.js
@@ -0,0 +1,31 @@
+// Ternary operator
+
+var p=20;
+var q=20;
+var r="20";
+var s=5;
+
+// condition ?  first case : second case
+
+// condition -> true : first case
+// condition -> false : second condition
+
+p == r ?  console.log("Success") : console.log("Failure");
+p === r ? console.log("Success") : console.log("Failure");
+
+
+// Nested ternary operator
+
+var p = 20;
+var q = 15;
+var r = 5;
+var s = 4;
+
+// post increment
+// post decrement 
+// pre increment
+// pre decrement
+
+
+
+
