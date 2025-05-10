4aaca128944f8ae348e7e2e3b7c2434f0adc4ea6
Pruthviraj Dadhaniya <pruthavirajdadhaniya@gmail.com>
2025-05-10 10:44:53 +0000

    nested-ternary-operator

--- operators/ternary-operator.js
+++ operators/ternary-operator.js
@@ -22,17 +22,34 @@ var r = 5;
 var s = 4;
 
 // post increment
-    console.log(p++);
+    console.log(p++); //current 20  // after 21
+    console.log(p);
     
 // post decrement
-    console.log(q--));
+    console.log(q--); // current 15  // after 14
+    console.log(q);
     
 // pre increment
-console.log(++r);
-
+    console.log(++r); // current 6 // after 6
+    console.log(r);
+    
 // pre decrement
-    console.log(--r);
+    console.log(--r); // current 3 // after 3  
     
 
+// Example
+
+var a = 1;
+var b = 2;
+var c;
+var d;
+
+c = ++b;    // b = 3  b = 3 // c = 3  
+d = a++;    // a = 1  a = 2 // d = 1
+c++;        // c = 3  c = 41
+b++;        // b = 3  b = 4
+++a;        // a = 3
+
+console.log(a,b,c,d); // 3 4 4 1
 
 
