93de2375bc240bbc02ecf417ca313f1de0341ae5
Pruthviraj Dadhaniya <pruthavirajdadhaniya@gmail.com>
2025-05-12 12:30:44 +0000

    string use case methods

--- string/string.js
+++ string/string.js
@@ -25,5 +25,16 @@ var family = "Maratha family";
 var place = "shivnari fort";
 var state = "Maharashtra";
 
+// method 1
 var result = "On " + dob + ", " + person + " was born in a " + family + " at " + place + ", ";
 console.log(result);
+
+// method 2 (concat)
+var result = "On ".concat(dob,", ", person, " Was born in a ", family, " at ", place, ", ", state,".");
+console.log(result);
+
+// method 3(Template Literals)
+var result = `On ${dob}, ${person}, Was born in a ${family} at ${place} ${place}, ${state}.`;
+console.log(result);
+
+
