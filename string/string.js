7cb8c0035c6786d4dd43f16acfaed525b0713309
Pruthviraj Dadhaniya <pruthavirajdadhaniya@gmail.com>
2025-05-12 12:08:20 +0000

    string and use case

---
+++ string/string.js
@@ -0,0 +1,29 @@
+// String 
+
+var fName = "pruthviraj";
+var lName = "Dadhaniya";
+
+// method 1(+)
+var result = fName +" "+ lName;
+console.log(result);
+
+// method 2 (concat)
+var result = fName.concat(" ",lName);
+console.log(result);
+
+// method 3 (Templete Literals)
+var result = `${fName} ${lName}`;
+console.log(result);
+
+
+// use case
+ var result = "On 19th Febuary 1630, chatrapati shivaji mahiraj was born in a maratha family at shivnari fort, maharashtra.";
+ 
+var dob = "19th Febuary 1630";
+var person = "chatrapati shivaji mahiraj";
+var family = "Maratha family";
+var place = "shivnari fort";
+var state = "Maharashtra";
+
+var result = "On " + dob + ", " + person + " was born in a " + family + " at " + place + ", ";
+console.log(result);
